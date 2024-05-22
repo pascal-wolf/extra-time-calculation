b6888d6ced0fd4db1f29cf35011ba52ad2eca0ec
pascal-wolf <pwolf96@icloud.com>
2024-05-22 23:48:34 +0200

    docs: :memo: add docstring

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,14 @@
 def create_text_color(predicted_class, prediction_proba):
+    """
+    Create text and color based on the predicted class.
+
+    Parameters:
+    predicted_class (int): The predicted class.
+    prediction_proba (float): The prediction probability.
+
+    Returns:
+    tuple: The text and color.
+    """
     if predicted_class == 1:
         text = "Out game "
         color = (60, 20, 220)
@@ -11,5 +21,15 @@ def create_text_color(predicted_class, prediction_proba):
 
 
 def calculate_extra_time(time_stop_counter, fps):
+    """
+    Calculate extra time.
+
+    Parameters:
+    time_stop_counter (int): The time stop counter.
+    fps (int): The frames per second.
+
+    Returns:
+    int: The extra time in seconds.
+    """
     seconds = time_stop_counter / fps
     return round(seconds)
